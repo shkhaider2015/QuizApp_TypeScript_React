e854ddd9afd895134cd3f787e5d8c6203843a3ac
Shakeel Haider <43218437+shkhaider2015@users.noreply.github.com>
2021-05-14 22:22:09 +0500

    kkjjk

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,17 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Quizcard } from './Components/Quizcard';
+import { CounterContextProvider } from './Context/ContextAPI';
+import DATA from "./Data/data.json";
 
 function App() {
+  // const answerCTX = React.useState(0)
   return (
     <div style={{ display: 'grid', height: '100vh', placeItems: 'center' }} >
-      
+     <CounterContextProvider>
         <Quizcard />
+     </CounterContextProvider>
+     
       
     </div>
   );
